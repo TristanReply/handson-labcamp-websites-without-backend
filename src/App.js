e0a59e0cb8dd41ea79b5342a90fbe2ef5a5d45d6
Cristiano Tarditi <c.tarditi@reply.it>
2018-12-06 18:56:04 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import "./index.css";
 
 // TODO - step 1: Import components
 
+import ProductsList from "./components/ProductsList";
+import AddProduct from "./components/AddProduct";
+import NewProduct from "./components/NewProduct";
+
 // TODO - step 4: Import Firebase and initialize it
 
 class App extends Component {
@@ -22,6 +26,7 @@ class App extends Component {
           </div>
         </div>
         {/* TODO - step 2: render ProductsList component */}
+        <ProductsList></ProductsList>
 
         {/* TODO - step 6: render NewProduct component */}
 
